a9b761c70c261cc345af21f509193e225fd32221
Z-imane <imanzgani1985@gmail.com>
2023-03-29 10:42:26 -0700

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncpy - function that copies a string
+ * @dest: destination value
+ * @src: source value
+ * @n: maximum numbers of bytes
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int index = 0, src_len = 0;
+while (src[index++])
+src_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[index] = src[index];
+for (index = src_len; index < n; index++)
+dest[index] = '\0';
+return (dest);
+}
