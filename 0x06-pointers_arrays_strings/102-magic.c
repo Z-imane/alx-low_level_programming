7728f0e2ed5607f71d7f1293c998efa9b6db81ca
Z-imane <imanzgani1985@gmail.com>
2023-03-29 15:37:39 -0700

    task10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -11,7 +11,7 @@ int main(void)
 /*
 * write your line of code here...
 * Remember:
-* main -magic
+* main - magic
 * - you are not allowed to use a
 *   - you are not allowed to modify p
 *   - only one statement
